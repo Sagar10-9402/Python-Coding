ae9afaba06a32fd0a155d861b21fc2703476351e
Sagar10-9402 <74914991+Sagar10-9402@users.noreply.github.com>
2021-04-04 19:58:34 +0530

    Solved Quations
    in that file, I solved some equations

---
+++ quations.py
@@ -0,0 +1,74 @@
+# exersise of all quations 
+
+# 1) this is double backslash 
+
+print("this is double backslash \\\ ") 
+
+#2) 	these are the /\/\/\/\ mountaions 
+
+print("these are the /\\/\\/\\/\\ mountains ")
+
+#3) he is	awosome 
+
+print("He is \t awosome ")
+
+#4) \" '\n \t  \'
+print(("\\\" \\n \\t \\' "))
+
+
+#5 calcluator 
+
+print(2+3)
+print(2*5)
+print((2+3)*6 /5 %2)
+
+#6 emoji function 
+
+#7 int function 
+
+number1 =int(input ( "\t Enter First no "))
+number2 =int(input( " \t Enter Second no "))
+total = number1 + number2
+
+print("total is " + str(total ))
+
+#8 print function
+name = input(" Enter your Name \n")
+
+print("hello "+ name)
+
+
+#9 normal sequance 
+ #outpput 
+	#line A \n line B 
+	#line A \\b line B
+	#\" \'
+
+print(r"line A \n line B")
+
+print(r"line A \b line B")
+
+print("\\\" \\\' ")
+
+#10 two inputs 
+
+name, age= input(" Enter your name and age ").split()
+#Age = input ("Enter your age ")
+print(name)
+print(age)
+
+#10 string formating 
+
+
+name = "Sagar"
+age = 25
+
+print(f"Hello {name} your age is {age } ")
+
+# varibales
+
+number1 =25
+numbar2 = 52
+
+total = number1 + number2
+print(number1 + number2)
